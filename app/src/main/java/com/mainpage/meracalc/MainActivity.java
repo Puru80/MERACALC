92e5045bac8d084c0f5c1fe9605547a41d79e103
Puru80 <puru.agar99@gmail.com>
2020-08-06 17:07:33 +0530

    First Repo

--- app/src/main/java/com/mainpage/meracalc/MainActivity.java
+++ app/src/main/java/com/mainpage/meracalc/MainActivity.java
@@ -14,8 +14,7 @@ public class MainActivity extends AppCompatActivity
     EditText ed1 ;
     EditText ed2 ;
 
-    public List<String> arr = new ArrayList<>();
-    public String st = "",str = "";
+    public String st = "";
 
     public void btn_0(View view)
     {
@@ -107,90 +106,9 @@ public class MainActivity extends AppCompatActivity
     public void Result(View view)
     {
         String ques = ed1.getText().toString();
-        double num1,num2;
-        for(int i=0;i<ques.length();i++)
-        {
-            if(ques.charAt(i)=='+' || ques.charAt(i)=='-' || ques.charAt(i)=='x' ||
-            ques.charAt(i)=='/' || ques.charAt(i)=='%')
-            {
-                arr.add(str);
-                str = "";
-                str = str + ques.charAt(i);
-                arr.add(str);
-            }
-            else
-            {
-                str = str + ques.charAt(i);
-            }
-        }
+         Utils obj = new Utils(ques);
 
-        for(int i=(arr.size()-1);i>=0;i--)
-        {
-            if(arr.get(i).equals("%"))
-            {
-                num1 = Double.parseDouble(arr.get(i-1));
-                num2 = Double.parseDouble(arr.get(i+1));
-                Double ans = (num1/num2)*100;
-                String stnum = ans.toString();
-                arr.set(i-1,stnum);
-                arr.remove(i);
-                arr.remove(i+1);
-            }
-        }
-
-        for(int i=(arr.size()-1);i>=0;i--)
-        {
-            if(arr.get(i).equals("/"))
-            {
-                num1 = Double.parseDouble(arr.get(i-1));
-                num2 = Double.parseDouble(arr.get(i+1));
-                Double ans = num1/num2;
-                String stnum = ans.toString();
-                arr.set(i-1,stnum);
-                arr.remove(i);
-                arr.remove(i+1);
-            }
-        }
-        for(int i=(arr.size()-1);i>=0;i--)
-        {
-            if(arr.get(i).equals("x"))
-            {
-                num1 = Double.parseDouble(arr.get(i-1));
-                num2 = Double.parseDouble(arr.get(i+1));
-                Double ans = num1*num2;
-                String stnum = ans.toString();
-                arr.set(i-1,stnum);
-                arr.remove(i);
-                arr.remove(i+1);
-            }
-        }
-        for(int i=(arr.size()-1);i>=0;i--)
-        {
-            if(arr.get(i).equals("+"))
-            {
-                num1 = Double.parseDouble(arr.get(i-1));
-                num2 = Double.parseDouble(arr.get(i+1));
-                Double ans = num1+num2;
-                String stnum = ans.toString();
-                arr.set(i-1,stnum);
-                arr.remove(i);
-                arr.remove(i+1);
-            }
-        }
-        for(int i=(arr.size()-1);i>=0;i--)
-        {
-            if(arr.get(i).equals("-"))
-            {
-                num1 = Double.parseDouble(arr.get(i-1));
-                num2 = Double.parseDouble(arr.get(i+1));
-                double ans = num1-num2;
-                String stnum = Double.toString(ans);
-                arr.set(i-1,stnum);
-                arr.remove(i);
-                arr.remove(i+1);
-            }
-        }
-        ed2.setText(arr.get(0));
+        ed2.setText(obj.Compute());
 
     }
 
---
+++ app/src/main/java/com/mainpage/meracalc/Utils.java
@@ -0,0 +1,103 @@
+package com.mainpage.meracalc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils
+{
+    public List<String> arr = new ArrayList<>();
+    public String str = "";
+
+    double num1,num2;
+
+    public Utils(String ques)
+    {
+        for(int i=0;i<ques.length();i++)
+        {
+            if(ques.charAt(i)=='+' || ques.charAt(i)=='-' || ques.charAt(i)=='x' ||
+                    ques.charAt(i)=='/' || ques.charAt(i)=='%')
+            {
+                arr.add(str);
+                str = "";
+                str = str + ques.charAt(i);
+                arr.add(str);
+            }
+            else
+            {
+                str = str + ques.charAt(i);
+            }
+        }
+    }
+
+    public String Compute()
+    {
+        for(int i=(arr.size()-1);i>=0;i--)
+        {
+            if(arr.get(i).equals("%"))
+            {
+                num1 = Double.parseDouble(arr.get(i-1));
+                num2 = Double.parseDouble(arr.get(i+1));
+                Double ans = (num1/num2)*100;
+                String stnum = ans.toString();
+                arr.set(i-1,stnum);
+                arr.remove(i);
+                arr.remove(i+1);
+            }
+        }
+
+        for(int i=(arr.size()-1);i>=0;i--)
+        {
+            if(arr.get(i).equals("/"))
+            {
+                num1 = Double.parseDouble(arr.get(i-1));
+                num2 = Double.parseDouble(arr.get(i+1));
+                Double ans = num1/num2;
+                String stnum = ans.toString();
+                arr.set(i-1,stnum);
+                arr.remove(i);
+                arr.remove(i+1);
+            }
+        }
+        for(int i=(arr.size()-1);i>=0;i--)
+        {
+            if(arr.get(i).equals("x"))
+            {
+                num1 = Double.parseDouble(arr.get(i-1));
+                num2 = Double.parseDouble(arr.get(i+1));
+                Double ans = num1*num2;
+                String stnum = ans.toString();
+                arr.set(i-1,stnum);
+                arr.remove(i);
+                arr.remove(i+1);
+            }
+        }
+        for(int i=(arr.size()-1);i>=0;i--)
+        {
+            if(arr.get(i).equals("+"))
+            {
+                num1 = Double.parseDouble(arr.get(i-1));
+                num2 = Double.parseDouble(arr.get(i+1));
+                Double ans = num1+num2;
+                String stnum = ans.toString();
+                arr.set(i-1,stnum);
+                arr.remove(i);
+                arr.remove(i+1);
+            }
+        }
+        for(int i=(arr.size()-1);i>=0;i--)
+        {
+            if(arr.get(i).equals("-"))
+            {
+                num1 = Double.parseDouble(arr.get(i-1));
+                num2 = Double.parseDouble(arr.get(i+1));
+                double ans = num1-num2;
+                String stnum = Double.toString(ans);
+                arr.set(i-1,stnum);
+                arr.remove(i);
+                arr.remove(i+1);
+            }
+        }
+
+        return arr.get(0);
+    }
+}
